368c57500ace7fce3bbf2b9066d9b212ae38bede
Yoan <unlock.eagle@gmail.com>
2025-05-10 21:15:54 +0200

    fix: auth.js

--- src/Middleware/auth.js
+++ src/Middleware/auth.js
@@ -1,20 +1,41 @@
-//Funcion que verifica si el usuario es admin
+// //Funcion que verifica si el usuario es admin
 
+// const soloAdmin = (req, res, next) => {
+//     console.log(req.user.role)
+//     if (req.user.role === "admin") {
+//         next();
+//     } else {
+//         res.status(403).send("Acceso denegado, Pa` donde vas pajarito!!");
+//     }
+    
+// };
+
+// const soloUser = (req, res, next) => {
+//     if (req.user.role === "user") {
+//         next();
+//     } else {
+//         res.status(403).send("Acceso denegado, Perteneces a un lugar mejor!!");
+//     }
+// };
+
+// export { soloAdmin, soloUser };
+
+// Función que verifica si el usuario es admin
 const soloAdmin = (req, res, next) => {
-    console.log(req.user.role)
-    if (req.user.role === "admin") {
-        next();
-    } else {
-        res.status(403).send("Acceso denegado, Pa` donde vas pajarito!!");
+    // Verifica que req.user exista y tenga el rol
+    if (!req.user || req.user.role !== "admin") {
+        return res.status(403).json({ error: "Acceso denegado, Pa` donde vas pajarito!!" });
     }
+    next();
 };
 
+// Función que verifica si el usuario es un usuario normal
 const soloUser = (req, res, next) => {
-    if (req.user.role === "user") {
-        next();
-    } else {
-        res.status(403).send("Acceso denegado, Perteneces a un lugar mejor!!");
+    // Verifica que req.user exista y tenga el rol
+    if (!req.user || req.user.role !== "user") {
+        return res.status(403).json({ error: "Acceso denegado, Perteneces a un lugar mejor!!" });
     }
+    next();
 };
 
 export { soloAdmin, soloUser };
